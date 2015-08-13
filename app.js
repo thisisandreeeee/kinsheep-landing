7a54bfbc2837993e35cce33042dba96cabcc0a41
Andre Tan <tanandre93@gmail.com>
2015-08-14 00:29:34 +0800

    cleanup stack

--- app.js
+++ app.js
@@ -1,70 +1,36 @@
-
-
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/user');
+var fs = require('fs');
 
 var app = express();
 
-var env = process.env.NODE_ENV || 'development';
-app.locals.ENV = env;
-app.locals.ENV_DEVELOPMENT = env == 'development';
-
-// view engine setup
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// app.use(favicon(__dirname + '/public/img/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/index.html');
 });
 
-/// error handlers
+app.get('/confirm', function (req, res) {
+	var email = req.query.email;
+	if (validEmail(email)) {
+		var file = fs.readFileSync(__dirname + '/signups.json', {encoding:'utf-8'});
+		fileJSON = JSON.parse(file);
+		if (fileJSON.indexOf(email) < 0) {
+			fileJSON.push(email);
+			fs.writeFileSync(__dirname + '/signups.json', JSON.stringify(fileJSON));
+			res.send("Signed Up!")
+		} else {
+			res.status(400).send("Already Signed Up.");
+			return;
+		}
+		
+	}
+	res.status(400).send("Invalid Email.");
+});
 
-// development error handler
-// will print stacktrace
+app.listen(3000);
 
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err,
-            title: 'error'
-        });
-    });
+function validEmail(email) {
+	var re = /[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}/igm;
+	if (email == '' || !re.test(email)) {
+		return false;
+	}
+	return true;
 }
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {},
-        title: 'error'
-    });
-});
-
-module.exports = app;
