33da700fa67bf0e445df874b8c15dc5210272fc5
Andre Tan <tanandre93@gmail.com>
2015-08-14 11:06:29 +0800

    upload initial canvas template

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var fs = require('fs');
+var path = require('path');
 
 var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/index.html');
