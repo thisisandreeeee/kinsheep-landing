aada72832010e0277ea380db321a00f1d7195172
Andre Tan <tanandre93@gmail.com>
2015-08-14 17:33:24 +0800

    fix email responsive box issue

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.get('/confirm', function (req, res) {
 	res.status(400).send("Invalid Email.");
 });
 
-app.listen(3000);
+app.listen(80);
 
 function validEmail(email) {
 	var re = /[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}/igm;
