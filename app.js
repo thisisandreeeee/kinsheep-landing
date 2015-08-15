87f5cb3d9f9e8094bd5316db770867d4e850c8d3
Andre Tan <tanandre93@gmail.com>
2015-08-16 01:21:48 +0800

    success page not working

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.get('/privacy-policy', function(req,res) {
 	res.sendFile(path.join(__dirname, '/views', '/privacy-policy.html'));
 });
 
-app.listen(3000);
+app.listen(80);
 
 function validEmail(email) {
 	var re = /[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}/igm;
