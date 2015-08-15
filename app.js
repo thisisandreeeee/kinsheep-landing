142544af115b40dd5b2eeb40503637f9375530a8
Andre Tan <tanandre93@gmail.com>
2015-08-16 01:15:10 +0800

    add basic views to success and duplicate pages

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 
 app.get('/', function (req, res) {
@@ -20,25 +21,25 @@ app.get('/confirm', function (req, res) {
 		if (fileJSON.indexOf(email) < 0) {
 			fileJSON.push(email);
 			fs.writeFileSync(__dirname + '/signups.json', JSON.stringify(fileJSON));
-			res.send("Signed Up!");
+			res.sendFile(path.join(__dirname, '/views', '/success.html'));
 		} else {
-			res.status(400).send("Already Signed Up.");
+			res.status(400).sendFile(path.join(__dirname, '/views', '/duplicate.html'));
 			return;
 		}
 		
 	}
-	res.status(400).send("Invalid Email.");
+	res.status(400).sendFile(path.join(__dirname, '/views', '/invalid.html'));;
 });
 
 app.get('/terms', function(req,res) {
-	res.send("Terms");
+	res.sendFile(path.join(__dirname, '/views', '/terms.html'));
 });
 
 app.get('/privacy-policy', function(req,res) {
-	res.send("Privacy Policy");
+	res.sendFile(path.join(__dirname, '/views', '/privacy-policy.html'));
 });
 
-app.listen(80);
+app.listen(3000);
 
 function validEmail(email) {
 	var re = /[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}/igm;
